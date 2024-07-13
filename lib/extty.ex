7b0a62676c3ef836be57f873400eca87edeafd90
Frank Hunleth <fhunleth@troodon-software.com>
2024-07-12 21:23:24 -0400

    Use both dot_iex and dot_iex_path to be safe
    This also updates the regression test to verify that the iex.exs
    definitely was run.

--- lib/extty.ex
+++ lib/extty.ex
@@ -122,20 +122,20 @@ defmodule ExTTY do
       # we want to support the options so we're using the mostly
       # private API :iex.start/2 which takes the options as a
       # keyword list and an MFA (which is same one used in :iex.start/0).
-      #
-      # Previously, the shell opts needed to be a double list since it
-      # was passed directly to an MFA, so we need to flatten it here.
-      # Then we also normalize the dot_iex_path arg to match the Elixir 1.17
-      # change to normalize the arg across the code
-      shell_opts =
+      flat_opts =
         opts
         |> List.flatten()
-        |> Enum.map(fn
-          {:dot_iex_path, path} -> {:dot_iex, path}
-          kv -> kv
+        |> Enum.reduce([], fn
+          {:dot_iex_path, path}, acc ->
+            # Elixir 1.17 uses :dot_iex_path and Elixir 1.18 uses :dot_iex,
+            # so pass both.
+            [{:dot_iex_path, path}, {:dot_iex, path} | acc]
+
+          kv, acc ->
+            [kv | acc]
         end)
 
-      {:iex, :start, [shell_opts, {:elixir_utils, :noop, []}]}
+      {:iex, :start, [flat_opts, {:elixir_utils, :noop, []}]}
     end
   else
     defp shell_spawner(%{type: :elixir, shell_opts: opts}) do
